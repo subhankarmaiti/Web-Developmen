f93f7eb7e278140a7941655746fb955081f4b949
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-11-22 22:37:52 +0530

    events slug filter

--- nextjs/events-app/components/events/events-search.js
+++ nextjs/events-app/components/events/events-search.js
@@ -27,17 +27,17 @@ function EventsSearch(props) {
           <label htmlFor="month">Month</label>
           <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
-            <option value="1">February</option>
-            <option value="1">March</option>
-            <option value="1">April</option>
-            <option value="1">May</option>
-            <option value="1">June</option>
-            <option value="1">July</option>
-            <option value="1">August</option>
-            <option value="1">September</option>
-            <option value="1">October</option>
-            <option value="1">November</option>
-            <option value="1">December</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
           </select>
         </div>
       </div>
--- nextjs/events-app/pages/events/[...slug].js
+++ nextjs/events-app/pages/events/[...slug].js
@@ -1,7 +1,40 @@
+import EventList from "../../components/events/event-list";
+import { getFilteredEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
+
 function FilteredEvents() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+  const numYear = +filterData[0];
+  const numMonth = +filterData[1];
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your value</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+  console.log(numYear, numMonth);
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events for the chosen filter</p>;
+  }
+
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 }
