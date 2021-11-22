a4e2863d044bc98aab0c8f41474ff43f82df22bf
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-11-22 11:15:42 +0530

    event details screen

--- nextjs/events-app/pages/events/[eventid].js
+++ nextjs/events-app/pages/events/[eventid].js
@@ -1,8 +1,29 @@
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistic from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+
 function EventDetail() {
+  const router = useRouter();
+  const { eventid } = router.query;
+  const event = getEventById(eventid);
+  if (!event) {
+    return <p>No event found</p>;
+  }
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistic
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
 
